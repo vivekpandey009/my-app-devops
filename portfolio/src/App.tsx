c63599f2d9d015304fb7829810d4b378b3e41665
vivekpandey009 <vivek.vidyamandir@gmail.com>
2026-06-17 19:37:45 +0530

    New version 3

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>DevOps Tutorial now with version 2</h1>
+          <h1>DevOps Tutorial now with version 2-1</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
